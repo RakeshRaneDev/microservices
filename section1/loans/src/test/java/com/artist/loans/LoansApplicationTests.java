bc35a0f76b8dbd1ec336af9d17f9e35213bb41d7
RakeshRaneDev <rakeshrane2017@gmail.com>
2025-03-04 20:14:22 +0530

    loan and card service

---
+++ section1/loans/src/test/java/com/artist/loans/LoansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artist.loans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoansApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
