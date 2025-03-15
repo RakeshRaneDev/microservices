c9ce469204792ce7e80ea5851ba455f9c825413c
Rakesh Rane <rakesh@Rakeshs-MacBook-Air.local>
2025-03-15 19:14:43 +0530

    service discovery

---
+++ banking_system/accounts/src/main/java/com/artist/accounts/service/ICustomersService.java
@@ -0,0 +1,13 @@
+package com.artist.accounts.service;
+
+import com.artist.accounts.dto.CustomerDetailsDto;
+
+public interface ICustomersService {
+
+    /**
+     *
+     * @param mobileNumber - Input Mobile Number
+     * @return Customer Details based on a given mobileNumber
+     */
+    CustomerDetailsDto fetchCustomerDetails(String mobileNumber);
+}
---
+++ banking_system/accounts/src/main/java/com/artist/accounts/service/impl/CustomersServiceImpl.java
@@ -0,0 +1,56 @@
+package com.artist.accounts.service.impl;
+
+import com.artist.accounts.dto.AccountsDto;
+import com.artist.accounts.dto.CardsDto;
+import com.artist.accounts.dto.CustomerDetailsDto;
+import com.artist.accounts.dto.LoansDto;
+import com.artist.accounts.entity.Accounts;
+import com.artist.accounts.entity.Customer;
+import com.artist.accounts.exception.ResourceNotFoundException;
+
+import com.artist.accounts.mapper.AccountMapper;
+import com.artist.accounts.mapper.CustomerMapper;
+import com.artist.accounts.repository.AccountRepository;
+import com.artist.accounts.repository.CustomerRepository;
+import com.artist.accounts.service.ICustomersService;
+import com.artist.accounts.service.client.CardsFeignClient;
+import com.artist.accounts.service.client.LoansFeignClient;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CustomersServiceImpl implements ICustomersService {
+
+    private AccountRepository accountsRepository;
+    private CustomerRepository customerRepository;
+    private CardsFeignClient cardsFeignClient;
+    private LoansFeignClient loansFeignClient;
+
+    /**
+     * @param mobileNumber - Input Mobile Number
+     * @return Customer Details based on a given mobileNumber
+     */
+    @Override
+    public CustomerDetailsDto fetchCustomerDetails(String mobileNumber) {
+        Customer customer = customerRepository.findByMobileNumber(mobileNumber).orElseThrow(
+                () -> new ResourceNotFoundException("Customer", "mobileNumber", mobileNumber)
+        );
+        Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId()).orElseThrow(
+                () -> new ResourceNotFoundException("Account", "customerId", customer.getCustomerId().toString())
+        );
+
+        CustomerDetailsDto customerDetailsDto = CustomerMapper.mapToCustomerDetailsDto(customer, new CustomerDetailsDto());
+        customerDetailsDto.setAccountsDto(AccountMapper.mapToAccountsDto(accounts, new AccountsDto()));
+
+        ResponseEntity<LoansDto> loansDtoResponseEntity = loansFeignClient.fetchLoanDetails(mobileNumber);
+        customerDetailsDto.setLoansDto(loansDtoResponseEntity.getBody());
+
+        ResponseEntity<CardsDto> cardsDtoResponseEntity = cardsFeignClient.fetchCardDetails(mobileNumber);
+        customerDetailsDto.setCardsDto(cardsDtoResponseEntity.getBody());
+
+        return customerDetailsDto;
+
+    }
+}
